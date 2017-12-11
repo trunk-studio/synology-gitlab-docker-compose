5ea5c417fbea4f33de0493d0d754d755bc9d8e35
smlsunxie <smlsun@gmail.com>
2017-12-11 11:46:50 +0800

    update

---
+++ start-gitlab.sh
@@ -0,0 +1,20 @@
+docker stop gitlab-mysql gitlab-redis gitlab
+docker rm gitlab-mysql gitlab-redis gitlab
+
+docker run --name gitlab-mysql -d \
+    --env-file ./gitlab.env \
+    --volume /volume1/docker/mysql:/var/lib/mysql \
+    -p 3336:3306 \
+    sameersbn/mysql:latest
+    
+docker run --name gitlab-redis -d \
+    --volume /volume1/docker/redis:/var/lib/redis \
+    sameersbn/redis:latest
+    
+docker run --name gitlab  \
+    --link gitlab-mysql:mysql \
+    --link gitlab-redis:redisio \
+    --publish 30001:22 --publish 30000:80 \
+    --env-file ./gitlab.env \
+    --volume /volume1/docker/gitlab:/home/git/data \
+    sameersbn/gitlab:10.2.2
